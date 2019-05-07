5567db5e8e0a1be9148530509e693398841edb26
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-07 22:38:36 +0300

    state, setState, handleChange, handleSubmit are created

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ class App extends React.Component {
           <div className="App__Aside"></div>
           <div className="App__Form">
 
+            {/* Page Switcher */}
             <div className="PageSwitcher">
               <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
               <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
             </div>
 
+            {/* Form Title Switcher */}
             <div className="FormTitle">
               <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active "className="FormTitle__Link">Sign Up</NavLink>
             </div>
--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -2,21 +2,56 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class SignInForm extends React.Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            email: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(e) {
+        // input
+        let target = e.target
+
+        // value of the input
+        let value = target.type === 'checkbox' ? target.checked : target.value
+        
+        // name of the input, is it FulName, Password or Email
+        let name = target.name
+
+        this.setState ({
+            [name]: value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        console.log('The form was submitted with the following data:')
+        console.log(this.state) 
+        //it will show up what ever user wrote in Email and Password Section
+    }
+
     render(){
         return (
             <div className="FormCenter">
-                <form className="FormFields" onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
               
                     {/*E-MAIL ADDRESS*/}
                     <div className="FormField">
                         <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
+                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
                     </div>
                           
                     {/*PASSWORD*/}
                     <div className="FormField">
                         <label className="FormField__Label" htmlFor="password">Password</label>
-                        <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+                        <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange}/>
                     </div>
 
               
